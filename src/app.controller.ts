a868e7131cb649056b0cd5ce6c19ae5e3b162dce
Power70 <nwisuanu@gmail.com>
2025-03-08 20:52:28 +0100

    learn about controller

---
+++ src/app.controller.ts
@@ -0,0 +1,25 @@
+import { Controller, Get, Post } from '@nestjs/common';
+import { AppService } from './app.service';
+import { Redirect, Query } from '@nestjs/common';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+  @Post()
+  addNewCat(): string {
+    return 'welcome to the cat world!';
+  }
+
+  @Get('docs')
+  @Redirect('https://docs.nestjs.com', 302)
+  getDocs(@Query('version') version) {
+    if (version && version === '5') {
+      return { url: 'https://docs.nestjs.com/v5/' };
+    }
+  }
+}
